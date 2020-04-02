7c4b52cafd32a6feb4a802970f07379aec06a698
Thomas Grubbe Sandborg Lauritsen <Laurits1994@gmail.com>
2020-04-02 12:21:43 +0200

    Josefine/Charlotte changes
    homebrew fix

--- ratemovies/server.js
+++ ratemovies/server.js
@@ -7,6 +7,8 @@ const port = 3000;
 var movieList = require('./data_2.json');
 var moiveRatings = require('./ratings_data.json');
 
+var user_genre; 
+
 const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
@@ -14,21 +16,41 @@ const server = http.createServer((req, res) => {
             res.write(fs.readFileSync('./ratemovies.html'));
             res.end();
             break;
-        // case sættes ind i server der allerede er 
+
         case '/submit':
             req.on('data', (data) => {
                 data = decodeURI(data);
-                data = data.split(/=|&/); //splitter strengen mellem tegnene
+                data = data.split(/=|&/); //splitter strengen mellem tegnene, så der kommer et array med genre brugeren har valgt
                 data = data.filter((e) =>{
                     return e != 'genre';
                 }) //filter tager en funktion ind, tjekker hvert element, hvis det er true ryger det ind i et array
                    // ellers smides det væk. her smides alle der ikke er 'genre' væk. 
-                console.log(data);
-                let user_genre = data; 
+                user_genre = data; 
                 
                 console.log(user_genre); //test
+                
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(fs.readFileSync('./ratemovies2.html'));
+
                 res.end();
             });
+            break;
+            
+        case '/rate':
+            res.write(JSON.stringify(find_movies(user_genre, movieList)));
+            res.end();
+            break;
+
+        case '/rate2':
+            req.on('data', (data) => {
+                res.end();
+                data = decodeURI(data);
+                data = JSON.parse(data);
+                //console.log(data);
+                user_rates_movies(3, data); 
+            });
+            break; 
+
         default:
             break;
     }
@@ -41,7 +63,7 @@ function run_server(){
     }); 
 }
 
-//run_server();
+run_server();
 
 
 function find_movies(user_genre, movieList){
@@ -51,24 +73,30 @@ function find_movies(user_genre, movieList){
     // finde film med de genre brugeren kan lide 
     for (let i = 0; i < movieList.length; i++) {
         let current_movie_genre = movieList[i].genres.split("|"); //genrene i filmen findes
-        console.log(current_movie_genre);
+        //console.log(current_movie_genre); test
 
-        //vi vælger film der har antal genre brugeren har valgt -1 (for mere variation)
+        /* //vi vælger film der har antal genre brugeren har valgt -1 (for mere variation)
         if (compare(user_genre, current_movie_genre).length === (user_genre.length - 1)) {  
-                same_genre_movies.push(movieList[i].movieId); //array med film id
-            } 
+                same_genre_movies.push({id: movieList[i].movieId, title: movieList[i].title}); //array med film id
+            } */
+
+         //vi vælger film der har antal genre brugeren har valgt -1 (for mere variation)
+         if (compare(user_genre, current_movie_genre).length === ((user_genre.length > 1) ? (user_genre.length - 1) : user_genre.length)) {  
+            same_genre_movies.push({id: movieList[i].movieId, title: movieList[i].title, genre: movieList[i].genres}); //array med film id
+        } 
+
     }
-    
     //vælger 10 random film fra same_genre_movies og sætter dem ind i movies_to_rate
     for (let i = 0; i < 10; i++) {
         movies_to_rate[i] = same_genre_movies[Math.floor(Math.random() * same_genre_movies.length)]; 
     }
+
+    console.log(movies_to_rate);
     return movies_to_rate;
 }
 
 
 //ser hvor mange ens genre der er, returnerer et array med dem
-
 function compare(user_genre_array, movie_genre_array){
     let finalarray = []; 
     user_genre_array.forEach((user_genre)=>movie_genre_array.forEach((movie_genre)=>{
@@ -80,25 +108,17 @@ function compare(user_genre_array, movie_genre_array){
     return finalarray; 
 }
 
-console.log(find_movies([ 'Comedy' , 'Romance' , 'Action'], movieList)); 
-
 //funktion der laver objekter med movie ratings til bestemt user 
-function user_rates_movies(user_id, movies_to_rate){
-    let rating = 0; 
-    let user_rating = {}; 
 
-    for (let i = 0; i <= movies_to_rate.length; i++) {
-        rating = window.prompt("Rate " + movieList[movies_to_rate[i]].title + "from 1-5: "); 
-        user_rating = {userId: user_id, movieId : movies_to_rate[i], rating : rating, timestamp : 00}; //nyt objekt
-        //gem i data fil
-        //gem i user 
-    }   
+function user_rates_movies(user_id, movies_to_rate){ 
+    let user_rating = [];
+    let test = require('./test.json'); //test skal erstattes, skal være anden variabel end moiveRatings, fordi den skal hentes hver gang den køres
+
+    for (let i = 0; i < movies_to_rate.length; i++) {
+       user_rating[i] = {userId: user_id, movieId : movies_to_rate[i].id, rating : movies_to_rate[i].rating, timestamp : 00}; //nyt objekt
+       test.push(user_rating[i]);
+        //gem i user ?
+    } 
+    fs.writeFileSync('./test.json', JSON.stringify(test).replace(/},{/g, "},\n{"));
 }
 
-    /*
-        spørgsmål til gruppen:
-        hvor skal genre gemmes henne i brugerprofil
-        hjælp til at gemme ratings i json-fil + brugerprofilen
-        billeder af filmens når brugeren skal rate
-    */
-            
